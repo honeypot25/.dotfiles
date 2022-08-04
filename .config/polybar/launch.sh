c12863aded7f05b78f392c0d240743f1ffc186cf
Andrea <40872160+honeypot25@users.noreply.github.com>
2022-08-04 23:36:33 +0200

    autocommit

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,4 +8,3 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar main --config=~/.config/polybar/config.ini &
-polybar second --config=~/.config/polybar/config.ini &

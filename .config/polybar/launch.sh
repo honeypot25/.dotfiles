0117697d8a3154664401d08639cee3b6d07f744b
honeypot25 <40872160+honeypot25@users.noreply.github.com>
2022-08-06 22:45:25 +0200

    autocommit

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,8 +3,6 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar main --config=~/.config/polybar/config.ini &
+echo "---" | tee -a /tmp/polybar.log
+# override monitor name for XRandR 1.5+ randomized monitor names
+MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g') polybar main 2>&1 | tee -a /tmp/polybar.log & disown

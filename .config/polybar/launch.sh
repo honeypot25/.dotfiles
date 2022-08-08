5b8da9599095076b956595e267ab4422c3a53285
honeypot25 <40872160+honeypot25@users.noreply.github.com>
2022-08-08 05:07:10 +0200

    autocommit

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,24 +10,20 @@ echo "---" | tee -a /tmp/polybar.log
 MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g') # fallback eDP1
 
 function network() {
-  # fallback: ETH_NIC
-  read -r ETH_NIC eth_UP WIFI_NIC wifi_UP < <(ip link | rg "^\d: ([ew]\w+): .+ state (\w+)" -or '$1 $2')
-  LABEL_CONNECTED="%{F#6C77BB}%{F-} %upspeed% %{F#6C77BB}%{F-} %downspeed%"
-  if [ "ETH_NIC" = "UP" ] && [ "WIFI_NIC" = "UP" ]; then
-    FCP="📶 / "
-    LABEL_CONNECTED=": ETH / WiFi"
-  elif [ "$eth_UP" = "UP" ]; then
+  read -r ETH_NIC eth_up WIFI_NIC wifi_up < <(ip link | rg "^\d: ([ew]\w+):.+ state (\w+)" -or '$1 $2')
+  if [ "eth_up" = "UP" ] && [ "wifi_up" = "UP" ]; then
     ACTIVE_NIC="$ETH_NIC"
-    FCP="📶"
-    FDP="🚫📶"
-  elif [ "$wifi_UP" = "UP" ]; then
+    LABEL_CONNECTED=" E|W %{F#6C77BB}%{F-} %downspeed%"
+  elif [ "$eth_up" = "UP" ]; then
+    ACTIVE_NIC="$ETH_NIC"  
+    LABEL_CONNECTED=" E %{F#6C77BB}%{F-} %downspeed%"
+  elif [ "$wifi_up" = "UP" ]; then
     ACTIVE_NIC="$WIFI_NIC"
-    FCP=""
-    FDP=""
-  else
-    FDP="✈️"
+    LABEL_CONNECTED=" W %{F#6C77BB}%{F-} %downspeed%"
   fi
-};
+  
+  export ACTIVE_NIC ETH_NIC LABEL_CONNECTED
+}; network
 
 # start
 polybar main 2>&1 | tee -a /tmp/polybar.log & disown

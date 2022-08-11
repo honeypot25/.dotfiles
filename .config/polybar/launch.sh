1848558515cebd27324aec051581c3cba0d486dd
honeypot25 <40872160+honeypot25@users.noreply.github.com>
2022-08-11 04:06:07 +0200

    autocommit

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,8 +7,12 @@ echo -e "\n+++ +++ +++ $(date +"%a %d %b | %X") +++ +++ +++\n" | tee -a /tmp/pol
 ### env variables
 #DEFAULT_NIC=$(ip route | grep "^default" | head -n1 | cut -d' ' -f5)
 #WIFI_NIC=$(ip link | rg "^\d: (w\w+)" -or '$1')
-#MONITOR=$(polybar -m | head -n1 | cut -d':' -f1) # ext*. fallback: main
-read -d'\n' -r MAIN_MON EXT_MON HDMI_MON VIRT_MON < <(xrandr --query | tail -n+2 | rg "^(\w+) " -or '$1')
+
+# DEFAULT_MON & MAIN_MON
+read -d'\n' -r DEFAULT_MON MAIN_MON < <(polybar -m | head -n2 | cut -d':' -f1) # ext*. fallback: main
+# read -d'\n' -r MAIN_MON EXT_MON HDMI_MON VIRT_MON < <(xrandr --query | tail -n+2 | rg "^(\w+) " -or '$1')
+
+# ADAPTER & BATTERY
 read -d' ' -r ADAPTER BATTERY < <(ls /sys/class/power_supply/)
 
 function network() {
@@ -28,8 +32,8 @@ function network() {
 network
 
 ### env variables exports
+export DEFAULT_MON MAIN_MON
 export ADAPTER BATTERY
-export MAIN_MON EXT_MON
 export ACTIVE_NIC ETH_NIC #LABEL_CONNECTED
 
 # start (with automatic reloads after config.ini changes)
